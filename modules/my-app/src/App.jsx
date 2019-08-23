38a0651d107dc62495a112c4356093f5bc87c370
jiefeimiao <465113174@qq.com>
2019-08-23 16:56:16 +0800

    添加了许多小demo

---
+++ modules/my-app/src/App.jsx
@@ -0,0 +1,10 @@
+//应用根组建
+import React from 'react'
+import moduleName from './logo.svg'
+ export default function App() {
+   return (
+     <div>
+       <img src={moduleName} alt="" srcset=""/>
+     </div>
+   )
+ }
